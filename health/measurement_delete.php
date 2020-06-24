585a0865c1ead4f433526998f02cd1f9c6436ea1
tanikawa <ztanikawa@gmail.com>
2020-06-24 23:14:49 +0900

    #3 画像圧縮によりmeasurement一覧のパフォーマンスを改善

--- health/measurement_delete.php
+++ health/measurement_delete.php
@@ -16,16 +16,18 @@
         $result_db_delete = $stmt->rowCount();
         
         //File Delete
+        unlink('images/measurement_pics/thumb/' . $delete_filename);
         if(unlink('images/measurement_pics/' . $delete_filename)){
             $result_file_delete = 1;
+            echo 'images/measurement_pics/thumb/' . $delete_filename;
         }else{
             $result_file_delete = 0;
         }
         echo $result_db_delete;
         echo $result_file_delete;
         
-        header('Location: measurement.php?result='.$result_db_delete*$result_file_delete);
-        exit();
+        // header('Location: measurement.php?result='.$result_db_delete*$result_file_delete);
+        // exit();
     }
 
 
