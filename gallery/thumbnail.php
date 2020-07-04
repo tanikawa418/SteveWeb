7c122a93b8ad81fe594a275cc41b5cc7e835bbc3
tanikawa <ztanikawa@gmail.com>
2020-07-04 22:31:53 +0900

    Galleryの動的生成を実装

--- gallery/thumbnail.php
+++ gallery/thumbnail.php
@@ -1,9 +1,11 @@
 <?php
+
+if(isset($_GET['file'])){
+
     ini_set("gd.jpeg_ignore_warning", 1);
-
-    $width = 1000; // 画像横幅を指定
-    $url = "images/2019-08-25 17.18.27_preview.jpeg"; // 画像URLを指定
-
+    
+    $width = 500; // 画像横幅を指定
+    $url = $_GET['file'];    
     list($image_w, $image_h) = getimagesize($url);
     $proportion = $image_w / $image_h;
     $height = $width / $proportion;
@@ -25,5 +27,6 @@
     imagecopyresampled($canvas, $image, 0, 0, 0, 0, $width, $height, $image_w, $image_h);
     imagejpeg($canvas, NULL, 100);
     imagedestroy($canvas);
+}
 
 ?>
